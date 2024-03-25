66d5bcb47a7ae0cf00cbd7796f483f221367751e
Vigneshwar <vigneshwaranandaram406@gmail.com>
2024-03-25 20:42:11 +0530

    Add files via upload

---
+++ Covid Project.sql
@@ -0,0 +1,38 @@
+/*
+
+Queries used for Tableau Project
+
+*/
+
+-- 1. Global Death percentage
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+-- 2. Total Death counts per continent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From CovidProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International', 'High income', 'Upper middle income', 'Lower middle income', 'Low income')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3. Percentage of population infected per country
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4. Percentage of population infected by date
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
